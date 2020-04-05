6dc7c7167739d26dacfa1473d602e40a6ccaa7f5
Manfrinmm <matheus_mm123@hotmail.com>
2020-04-05 01:32:05 -0300

    Refectory setInterval & insert countdown values

--- app.js
+++ app.js
@@ -9,7 +9,18 @@ const spinLoading = document.querySelector("#loading");
 
 const nextYearContainer = document.querySelector("#year");
 
-const datetest = new Date("2020-12-31 23:55:55");
+nextYearContainer.textContent = nextYear;
+
+function getTimeUnit(unit) {
+  return unit < 10 ? "0" + unit : unit;
+}
+
+function insertCountdownValues({ seconds, minutes, hours, days }) {
+  secondsContainer.textContent = getTimeUnit(seconds);
+  minutesContainer.textContent = getTimeUnit(minutes);
+  hoursContainer.textContent = getTimeUnit(hours);
+  daysContainer.textContent = getTimeUnit(days);
+}
 
 function updateCountdown() {
   const currentYear = new Date();
@@ -21,18 +32,15 @@ function updateCountdown() {
   const hours = Math.floor(difference / 1000 / 60 / 60) % 24;
   const days = Math.floor(difference / 1000 / 60 / 60 / 24);
 
-  secondsContainer.textContent = seconds < 10 ? "0" + seconds : seconds;
-  minutesContainer.textContent = minutes < 10 ? "0" + minutes : minutes;
-  hoursContainer.textContent = hours < 10 ? "0" + hours : hours;
-  daysContainer.textContent = days < 10 ? "0" + days : days;
-
-  nextYearContainer.textContent = nextYear;
+  insertCountdownValues({ seconds, minutes, hours, days });
 }
 
-setInterval(() => updateCountdown(), 1000);
-
-setTimeout(() => {
+function handleCountdownDisplay() {
   spinLoading.remove();
 
   countdownContainer.style.display = "flex";
-}, 1000);
+}
+
+setTimeout(handleCountdownDisplay, 1000);
+
+setInterval(() => updateCountdown(), 1000);
